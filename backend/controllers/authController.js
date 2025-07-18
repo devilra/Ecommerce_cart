1b80e044e64f406183a0f2e4fdd7a2f9affb3d8c
devilra <rockerraja906@gmail.com>
2025-07-18 19:31:44 +0530

    secure change

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -46,7 +46,7 @@ exports.login = async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         sameSite: "strict",
-        secure: false, // production: true with HTTPS
+        secure: true, // production: true with HTTPS
         maxAge: 7 * 24 * 60 * 60 * 1000,
       })
       .json({
